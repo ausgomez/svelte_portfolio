7819fa857cfd370e04ad5b4a11b94a3d81775a1a
Aus Gomez <austrebertog@gmail.com>
2020-05-23 20:31:25 -0500

    fixing video size

--- src/components/Intro.svelte
+++ src/components/Intro.svelte
@@ -2,9 +2,9 @@
 
 </script>
 
-<div class="text-center">
+<div class="container text-center video">
   <h1>Who I am</h1>
-  <div class="embed-responsive embed-responsive-16by9">
+  <div class="embed-responsive embed-responsive-1by1">
     <iframe class="embed-responsive-item"
         width="560"
         height="315"
@@ -15,3 +15,19 @@
         allowfullscreen />
     </div>
 </div>
+
+<style>
+.video{
+    width: 50%;
+    text-align: center;
+}
+
+@media only screen and (max-width: 800px){
+    .video{
+    width: 100%;
+    text-align: center;
+}
+}
+</style>
+
+
